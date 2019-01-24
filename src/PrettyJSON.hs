425a8847d715110e57119a3a6d80f7e5bd2a5954
Sérgio Souza Costa <prof.sergio.costa@gmail.com>
2019-01-23 23:10:55 -0300

    estrutura inicial da biblioteca

---
+++ src/PrettyJSON.hs
@@ -0,0 +1,11 @@
+module PrettyJSON where
+
+import SimpleJSON
+import Prettify
+
+renderJValue :: JValue -> Doc
+renderJValue (JBool True)  = text "true"
+renderJValue (JBool False) = text "false"
+renderJValue JNull         = text "null"
+renderJValue (JNumber num) = double num
+renderJValue (JString str) = string str
