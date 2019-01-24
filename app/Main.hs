ecc94bfc62c23cd977fd1e24fec9d29dd6db3963
Sérgio Souza Costa <prof.sergio.costa@gmail.com>
2019-01-24 00:37:03 -0300

    concluído as funcionalidades basicas

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,13 @@
 module Main (main) where
 
 import SimpleJSON
+import Prettify
+import PrettyJSON
 
-main = print (JObject [("foo", JNumber 1), ("bar", JBool False)])
+
+value = renderJValue $ JObject [("f", JNumber 1), ("q", JBool True)]
+main :: IO ()
+main = do
+    putStrLn (pretty 10 value)
+    putStrLn (pretty 20 value)
+    putStrLn (pretty 30 value)
